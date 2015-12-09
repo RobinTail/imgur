9fd825a0930abbf8bd5a8a1a40dcb32ee066382e
Robin Tail <robin_tail@list.ru>
2015-12-09 12:03:11 +0300

    Two scripts to run server

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var app = express();
 var routes = require('./routes');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpack = require('webpack');
-
-var webpackCompiler = webpack(require('./webpack.config'));
-
-function logger(req, res, next) {
-    console.log('Requested %s for %s', req.method, req.originalUrl);
-    next();
-}
-
-app.use(webpackDevMiddleware(webpackCompiler, {
-    noInfo: true
-}));
 
 // each request
 app.use(logger);
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Using webpack dev middleware');
+    var webpack = require('webpack');
+    var webpackDevMiddleware = require('webpack-dev-middleware');
+    var webpackCompiler = webpack(require('./webpack.config'));
+    app.get('/index.js', webpackDevMiddleware(webpackCompiler, {
+        noInfo: true
+    }));
+} else {
+    console.log('Using webpack build');
+}
 
 routes(app, express);
 
@@ -25,3 +24,8 @@ var server = app.listen(8080, function() {
     var port = server.address().port;
     console.log('Serving at %s:%s', host, port);
 });
+
+function logger(req, res, next) {
+    console.log('Requested %s for %s', req.method, req.originalUrl);
+    next();
+}
